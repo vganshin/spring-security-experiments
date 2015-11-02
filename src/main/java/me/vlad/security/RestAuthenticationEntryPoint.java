b22b07de6ced3997b28d9b17ebb63c3603299418
Vladislav Ganshin <ganshinv@gmail.com>
2015-11-02 15:09:03 +0300

    server returns 401 on the unauthorized request.

---
+++ src/main/java/me/vlad/security/RestAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package me.vlad.security;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Created by vganshin on 02.11.15.
+ */
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    public void commence(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
